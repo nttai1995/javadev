0d4315c4f1355096f76419670fa0f03d1a358379
Tai Nguyen Thanh <nttai.dt135160@gmail.com>
2020-10-29 16:08:52 +0700

    Update validation

--- Spring-mvc-thymeleaf/src/main/java/com/example/Springmvcthymeleaf/controller/EmployeeController.java
+++ Spring-mvc-thymeleaf/src/main/java/com/example/Springmvcthymeleaf/controller/EmployeeController.java
@@ -1,14 +1,25 @@
 package com.example.Springmvcthymeleaf.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import javax.validation.Valid;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.Springmvcthymeleaf.DTO.EmployeeDTO;
+import com.example.Springmvcthymeleaf.entity.Employee;
 import com.example.Springmvcthymeleaf.service.EmployeeService;
 
 @Controller
@@ -21,10 +32,82 @@ public class EmployeeController {
 		this.employeeService = employeeService;
 	}
 	
-	@GetMapping("/list")
-	public String getEmployeeSortByParam(@RequestParam(name = "sortBy", required = false, defaultValue = "email") String field, Model model) {
-		List<EmployeeDTO> employees = employeeService.getAllEmployees(field);
+	@GetMapping("")
+	public String getListEmployee(
+			@RequestParam(name = "sortBy", required = false, defaultValue = "email") String sortBy,
+			@RequestParam(name = "pageNo", required = false, defaultValue = "1") Integer pageNo,
+			@RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer pageSize,
+			Model model) 
+	{
+		Page<EmployeeDTO> employees = employeeService.getAllEmployees(sortBy, pageNo - 1, pageSize);
 		model.addAttribute("employees", employees);
-		return "employee";
+		int totalPages = employees.getTotalPages();
+		if(totalPages > 0) {
+			List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+					.boxed()
+					.collect(Collectors.toList());
+			model.addAttribute("pageNumbers",pageNumbers);
+		}
+		model.addAttribute("employee", new Employee());
+		return "employee/index";
 	}
+	
+	@GetMapping("/search")
+	public String findEmployeeByFirstName(
+			@RequestParam(name = "firstName", required = true) String firstName,
+			Model model) 
+	{
+		Page<EmployeeDTO> employees = new PageImpl<>(employeeService.findByFirstName(firstName)) ;
+		model.addAttribute("employees", employees);
+		model.addAttribute("employee", new Employee());
+		return "employee/index";
+	}
+	
+	@GetMapping("/detail")
+	public String findOneEmployee(
+			@RequestParam(name = "empId", required = true) Integer empId,
+			Model model) 
+	{
+		Employee employee = employeeService.findOne(empId);
+		model.addAttribute("employee", employee);
+		return "employee/detail";
+	}
+	
+	@PostMapping("")
+	public String addEmployee(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "employee/add";
+		}
+		else {
+			try {
+				employeeService.saveEmployee(employee);
+			} catch (DataIntegrityViolationException e) {
+				if(e.getMessage().contains("employees.email")) {
+					bindingResult.rejectValue("email", "error.user", "An account already exists for this email.");
+					return "employee/add";
+				}else {
+					throw e;
+				}
+			}
+			return "redirect:/employee";
+		}
+	}
+	
+	@GetMapping("/edit")
+	public String updateEmployee(
+			@RequestParam(name = "empId", required = true) Integer empId,
+			Model model) 
+	{
+		Employee employee = employeeService.findOne(empId);
+		model.addAttribute("employee", employee);
+		return "employee/edit";
+	}
+	
+	@PostMapping("/delete")
+	public String deleteEmployee(@RequestParam(name = "empId", required = true) Integer empId, Model model) {
+		employeeService.deleteEmployee(empId);
+		return "redirect:/employee";
+	}
+	
+	
 }
--- Spring-mvc-thymeleaf/src/main/java/com/example/Springmvcthymeleaf/entity/Employee.java
+++ Spring-mvc-thymeleaf/src/main/java/com/example/Springmvcthymeleaf/entity/Employee.java
@@ -6,41 +6,51 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "employeeNumber")
 	private Integer employeeNumber;
 	
+	@NotBlank
 	@Column(name = "lastName")
 	private String lastName;
 	
+	@NotBlank
 	@Column(name = "firstName")
 	private String firstName;
 	
+	@NotBlank
 	@Column(name = "extension")
 	private String extension;
 	
-	@Column(name = "email")
+	@NotBlank
+	@Email(regexp= "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+	@Column(name = "email", unique = true)
 	private String email;
 	
+	@NotBlank
 	@Column(name = "officeCode")
 	private String officeCode;
 	
 	@Column(name = "reportsTo", nullable=true)
 	private Integer reportsTo;
 	
+	@NotBlank
 	@Column(name = "jobTitle")
 	private String jobTitle;
 
-	public int getEmployeeNumber() {
+	public Integer getEmployeeNumber() {
 		return employeeNumber;
 	}
 
-	public void setEmployeeNumber(int employeeNumber) {
+	public void setEmployeeNumber(Integer employeeNumber) {
 		this.employeeNumber = employeeNumber;
 	}
 
